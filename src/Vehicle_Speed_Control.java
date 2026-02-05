4d314daef5a17f0a538030f2e66d31a0c3d954f3
madhanraghuram <madhanraghuram@gmail.com>
2026-02-05 22:15:48 +0530

    Added in OOP

---
+++ src/Vehicle_Speed_Control.java
@@ -0,0 +1,23 @@
+class Vehicle{
+private int speed=0;
+    void setSpeed(int value){
+        speed+=value;
+    }
+    int getSpeed(int speed){
+        return speed;
+    }
+void display(){
+    System.out.println(speed);
+}
+}
+
+public class Vehicle_Speed_Control extends Vehicle{
+    static void main(String[] args) {
+Vehicle in=new Vehicle();
+
+in.setSpeed((in.getSpeed(50)));
+        in.setSpeed((in.getSpeed(50)));
+        in.setSpeed((in.getSpeed(50)));
+        in.display();
+    }
+}
